9b3821f2bc0521aa63c36d868aad52543e682b80
AB <ab@chromium.org>
2020-12-29 23:51:48 +0800

    oauth2 login does not work

---
+++ backend/src/main/java/com/walle/gitBookHost/GitBookHostApplication.java
@@ -0,0 +1,13 @@
+package com.walle.gitBookHost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitBookHostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitBookHostApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/walle/gitBookHost/GitBookHostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walle.gitBookHost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitBookHostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
